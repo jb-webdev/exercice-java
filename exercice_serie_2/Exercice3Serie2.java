29dd1ec82256bf7045e9798a6e848b515b0f93a7
j.boero <boero.dw@gmail.com>
2021-07-25 16:29:09 +0200

    exercice algo 3 fini a ma sauce corriger

--- exercice_serie_2/Exercice3Serie2.java
+++ exercice_serie_2/Exercice3Serie2.java
@@ -2,33 +2,65 @@ public class Exercice3Serie2 {
     public static void main(String[] args) {
 		
 		// je cree un tableau pour simuler les nombre donné par l'utilisateur
-		int [] arrayUser = {10, 4, 3, 0, 10, 8, 25, 7, 38, 78, 209};
-
+		int [] arrayUser = {40, 12, 28, 32, 360, 0};
 		// je cree une variable pour stocker ma moyenne pour la fin
-		int resultat = 0;
+		int nb_userBefore = 0;
 
-		// Je cree une variable pour compter les nombre superieur a 0
-		int compteurNombreSuperieurAZero = 0;
-		
-		// je cree une compteurNombreSuperieurAZero for pour pour recuperer les valeur dans mon tableau
+		int sommeNumber = 0;
+		// Je cree une variable pour compter les nombre de boucle réalisée
+		int compteurNombre = 0;
+		int moyenneActuel = 0;
+
 		for (int userNumber : arrayUser){
 			
-			System.out.println("Donnez moi un nombre superieur a zero ?");
+			System.out.println("Donnez moi un nombre ?");
 			System.out.println("Votre nombre est " + userNumber);
 			
 			if (userNumber < 1 ){
-				System.out.println("Votre nombre n'est pas bon !");
+				System.out.println("la boucle vas s'arreter !");
 			} else {
-				System.out.println("Super ! je note !");
-				resultat = resultat + userNumber;
-				compteurNombreSuperieurAZero ++;
+				if (sommeNumber == 0 ){
+					System.out.println("c'est votre premier nombre !");
+					sommeNumber = sommeNumber + userNumber;
+					compteurNombre = compteurNombre +1;
+					moyenneActuel = sommeNumber / compteurNombre;
+					System.out.println("La moyenne actuel est de : " + moyenneActuel);
+					if (moyenneActuel > userNumber){
+						System.out.println("votre nombre est inferieur a la moyenne actuel !");
+					} else if (moyenneActuel < userNumber){
+						System.out.println("votre nombre est superieur a la moyenne actuel !");
+					}else if (moyenneActuel == userNumber){
+						System.out.println("votre nombre est egale a la moyenne actuel !");
+					}
+					nb_userBefore = userNumber;
+
+				} else {
+					sommeNumber = sommeNumber + userNumber;
+					compteurNombre = compteurNombre ++;
+					moyenneActuel = sommeNumber / compteurNombre;
+					System.out.println("La moyenne actuel est de : " + moyenneActuel);
+					if (moyenneActuel > userNumber){
+						System.out.println("votre nombre est inferieur a la moyenne actuel !");
+					} else if (moyenneActuel < userNumber){
+						System.out.println("votre nombre est superieur a la moyenne actuel !");
+					} else if (moyenneActuel == userNumber){
+						System.out.println("votre nombre est egale a la moyenne actuel !");
+					}
+
+					if(nb_userBefore == userNumber){
+						System.out.println("Vous m'avez donné le meme nombre que precedement !");
+					} else if (nb_userBefore < userNumber){
+						System.out.println("votre nombre est superieur a votre nombre precedent !");
+					} else if (nb_userBefore > userNumber){
+						System.out.println("votre nombre est inferieur a votre nombre precedent !");
+					}
+					nb_userBefore = userNumber;
+				}
+
 			}
 		}
-		System.out.println("la moyenne des nombres est de : " + (resultat / compteurNombreSuperieurAZero));
-		System.out.println("valeur compteur nombre superieur a zero :" + compteurNombreSuperieurAZero);
-		System.out.println("sommme des nombres de l'utilisateur :" + resultat);
-		
-		// javac Exercice3Serie2.java
-		// java Exercice3Serie2
-	}
+	
+				// javac Exercice3Serie2.java
+				// java Exercice3Serie2
+			}
 }
